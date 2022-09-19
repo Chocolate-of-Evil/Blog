f14a254e0683e8276306aa64b95307febc93f394
mac-mini-116 <mac-mini-116@Mac-mini-2.local>
2022-09-19 16:04:14 +0300

    fixing test

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -7,37 +7,40 @@ RSpec.describe '#user' do
     let(:user) { create(:user) }
 
     context 'with email and password' do
-      scenario 'is valid' do
+      before do
         visit new_user_session_path
-
         fill_in 'Email', with: user.email
         fill_in 'Password', with: user.password
         click_button 'Log in'
+      end
 
+      it 'is valid' do
         expect(page).to have_content 'Signed in successfully.'
       end
     end
 
     context 'without email' do
-      scenario 'is not valid' do
+      before do
         visit new_user_session_path
-
         fill_in 'Email', with: nil
         fill_in 'Password', with: user.password
         click_button 'Log in'
+      end
 
+      it 'is not valid' do
         expect(page).to have_content 'Invalid Email or password.'
       end
     end
 
     context 'without password' do
-      scenario 'is not valid' do
+      before do
         visit new_user_session_path
-
         fill_in 'Email', with: user.email
         fill_in 'Password', with: nil
         click_button 'Log in'
+      end
 
+      it 'is not valid' do
         expect(page).to have_content 'Invalid Email or password.'
       end
     end
