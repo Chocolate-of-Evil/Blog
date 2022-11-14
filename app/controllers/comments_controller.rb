0791acac3cd535443c15394193f285eed67c98c2
mac-mini-116 <mac-mini-116@Mac-mini-2.local>
2022-11-14 14:57:29 +0300

    Delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,11 +13,11 @@ class CommentsController < ApplicationController
     end
   end
 
-  def destroy
+  def show
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
-    redirect_to article_path(@article), status: :see_other
+    redirect_to article_path(@article)
   end
 
   private
